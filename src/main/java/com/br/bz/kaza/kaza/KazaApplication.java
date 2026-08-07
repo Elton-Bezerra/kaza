55ad6f35d38f9190930cd75a656e6cf891f5e3e8
Elton Bezerra <elton.cin@hotmail.com>
2026-08-07 16:59:34 -0300

    App scaffold

---
+++ src/main/java/com/br/bz/kaza/kaza/KazaApplication.java
@@ -0,0 +1,13 @@
+package com.br.bz.kaza.kaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KazaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KazaApplication.class, args);
+    }
+
+}
